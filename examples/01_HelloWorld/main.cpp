f84ccdc7941f606fad4f61ab904fb538546bac08
mogemimi <mogemimi@enginetrouble.net>
2017-01-15 12:53:01 +0900

    Add "Hello World" example

---
+++ examples/01_HelloWorld/main.cpp
@@ -0,0 +1,30 @@
+#include "SpellChecker.h"
+#include <iostream>
+
+using typopoi::SpellChecker;
+
+int main()
+{
+    SpellChecker spellChecker;
+    spellChecker.AddWord(u8"hello");
+    spellChecker.AddWord(u8"world");
+
+    auto suggest = [&](const std::string& word) -> std::string {
+        auto result = spellChecker.Suggest(word);
+        if (result.correctlySpelled) {
+            // NOTE: 'word' is correct.
+            return word;
+        }
+        if (result.suggestions.empty()) {
+            // NOTE: 'word' is not found in the directory.
+            return word;
+        }
+        // NOTE: 'word' is probably misspelled word.
+        return result.suggestions.front();
+    };
+
+    // NOTE: "Hello, world!"
+    std::cout << suggest(u8"Hell, would!") << std::endl;
+
+    return 0;
+}
